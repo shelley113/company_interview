d4e4e3101e3c1f073a1d25b6da8bf7b7888377e8
shelley chen <31610209+shelley113@users.noreply.github.com>
2018-09-17 11:49:39 +1000

    project design improvement
    split robot class into 3 classes

--- MyXplor/robot.rb
+++ MyXplor/robot.rb
@@ -1,5 +1,115 @@
 #!/usr/bin/ruby
 
+# Accept commond from file
+class Input
+
+    def initialize()
+        @validation = Validation.new
+        @robot = Robot.new
+        @operation = Operation.new(@robot)
+    end
+
+    def startToMove(fileName)
+        lines = File.readlines(fileName)
+        for commond in lines do
+            commonds = commond.split(" ")
+
+            if commonds[0] == "PLACE"
+                robotInformation = commonds[1].split(",")
+                if robotInformation[0].to_i >= 0 && robotInformation[0].to_i <= 4 && robotInformation[1].to_i >=0 && robotInformation[1].to_i <= 4
+                    @robot.positionX = robotInformation[0].to_i
+                    @robot.positionY = robotInformation[1].to_i
+                    @robot.direction = robotInformation[2]
+                end
+            else
+                if @validation.checkValid(@robot.positionX,@robot.positionY) 
+                    case commond.delete("\n")
+                    when "MOVE"
+                         @operation.operation
+                    when "LEFT"
+                         @operation.turnLeft
+                    when "RIGHT"
+                        @operation.turnRight
+                    when "REPORT"
+                         @robot.display_information
+                    else
+                         #Ignore
+                    end
+                end
+            end
+        end
+
+    end
+end
+
+# check if the robot position is valid
+class Validation
+    def checkValid(x,y)
+        if x != nil && y != nil
+            return true
+        else
+            return false
+        end
+
+    end
+end
+
+# robot movement
+class Operation
+
+    def initialize(robot)
+        @robot = robot
+    end
+
+    def operation
+        case @robot.direction
+        when "NORTH"
+            if @robot.positionY != 4
+                @robot.positionY = @robot.positionY + 1
+            end
+        when "SOUTH"
+            if @robot.positionY != 0
+                @robot.positionY = @robot.positionY - 1
+            end  
+        when "EAST"
+            if @robot.positionX != 4
+                @robot.positionX = @robot.positionX + 1
+            end
+        when "WEST"
+            if @robot.positionX != 0
+                @robot.positionX = @robot.positionX - 1
+            end    
+        end
+    end
+
+    def turnLeft
+        case @robot.direction
+        when "NORTH"
+            @robot.direction = "WEST"
+        when "SOUTH"
+            @robot.direction = "EAST" 
+        when "EAST"
+            @robot.direction = "NORTH"
+        when "WEST"
+            @robot.direction = "SOUTH"   
+        end
+    end
+
+    def turnRight
+        case @robot.direction
+        when "NORTH"
+            @robot.direction = "EAST"
+        when "SOUTH"
+            @robot.direction = "WEST" 
+        when "EAST"
+            @robot.direction = "SOUTH"
+        when "WEST"
+            @robot.direction = "NORTH"   
+        end
+    end
+end
+
+# robot information
 class Robot
 
     attr_accessor :positionX
@@ -9,94 +119,10 @@ class Robot
     def display_information
         puts "#{@positionX},#{@positionY},#{@direction}"
     end
-
-    def validation
-        if @positionX != nil && @positionY != nil
-            return true
-        else
-            return false
-        end
-
-    end
-
-    def operation
-        case @direction
-        when "NORTH"
-            if @positionY != 4
-                @positionY = @positionY + 1
-            end
-        when "SOUTH"
-            if @positionY != 0
-                @positionY = @positionY - 1
-            end  
-        when "EAST"
-            if @positionX != 4
-                @positionX = @positionX + 1
-            end
-        when "WEST"
-            if @positionX != 0
-                @positionX = @positionX - 1
-            end    
-        end
-    end
-
-    def turnLeft
-        case @direction
-        when "NORTH"
-            @direction = "WEST"
-        when "SOUTH"
-            @direction = "EAST" 
-        when "EAST"
-            @direction = "NORTH"
-        when "WEST"
-            @direction = "SOUTH"   
-        end
-    end
-
-    def turnRight
-        case @direction
-        when "NORTH"
-            @direction = "EAST"
-        when "SOUTH"
-            @direction = "WEST" 
-        when "EAST"
-            @direction = "SOUTH"
-        when "WEST"
-            @direction = "NORTH"   
-        end
-    end
-
-    def start(fileName)
-        lines = File.readlines(fileName)
-        for commond in lines do
-            if commond[0,5] == "PLACE"
-                if commond[6,7].to_i >= 0 && commond[6,7].to_i <= 4 && commond[8,9].to_i >=0 && commond[8,9].to_i <= 4
-                    @positionX = commond[6,7].to_i
-                    @positionY = commond[8,9].to_i
-                    @direction = commond[10,commond.length].delete("\n")
-                end
-            else
-                if validation 
-                    case commond.delete("\n")
-                    when "MOVE"
-                         operation
-                    when "LEFT"
-                         turnLeft
-                    when "RIGHT"
-                         turnRight
-                    when "REPORT"
-                         display_information
-                    else
-                         "Ignore"
-                    end
-                end
-            end
-        end
-
-    end
     
 end
 
-robot = Robot. new
-robot.start("data.rb")
+
+input = Input.new()
+input.startToMove("data.rb")
 
